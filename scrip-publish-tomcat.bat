328846fd87fa13c1c2fe1726f4ab3ac17da7723f
balita <balitatsiory@gmail.com>
2023-09-27 15:29:56 +0300

    nigga

---
+++ scrip-publish-tomcat.bat
@@ -0,0 +1,5 @@
+cd webappbalita/WEB-INF/classes
+javac -d . *.java
+jar -cf myjarobjet.jar ./
+move myjarobjet.jar ../lib
+pause
---
+++ webappbalita/WEB-INF/classes/Myobjet.java
@@ -0,0 +1,13 @@
+/**
+ * Myobjet
+ */
+package myobjet;
+
+public class Myobjet {
+    String namespace;
+
+    public Myobjet() {
+        this.namespace = "nigga";
+    }
+
+}
