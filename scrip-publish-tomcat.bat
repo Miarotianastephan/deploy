a6709b4b456954a6012d73ced1f6b027b4c5fa87
balita <balitatsiory@gmail.com>
2023-09-27 16:34:23 +0300

    realy finish

--- scrip-publish-tomcat.bat
+++ scrip-publish-tomcat.bat
@@ -1,5 +1,9 @@
+git pull origin dev2
 cd webappbalita/WEB-INF/classes
 javac -d . *.java
 jar -cf myjarobjet.jar ./
 move myjarobjet.jar ../lib
+cd ../../
+jar -cvf webappbalita.war ./
+copy "webappbalita.war" "C:\Program Files\Apache Software Foundation\Tomcat 9.0\webapps"
 pause
